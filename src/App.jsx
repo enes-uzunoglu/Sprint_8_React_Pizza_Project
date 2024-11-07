d3012ad63f172c9395e74e261643f73300aa5178
enes-uzunoglu <developer.enesuzunoglu@gmail.com>
2024-11-07 03:07:06 +0300

    Form elemanları çeşitlendir
    Ek malzemeler eklendi.
    Validasyon lar eklendi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import React from 'react'
-import './reset.css'
-import './App.css'
-import Header from './Header'
-import Form from './Form'
-
+import React from "react";
+import "./reset.css";
+import "./App.css";
+import Header from "./Header";
+import Form from "./Form";
 
 function App() {
-  
-
   return (
-
     <>
-
-    <Header> </Header>
-    <Form> </Form>
-    
-    
+      <Header> </Header>
+      <Form> </Form>
     </>
-
-  )
+  );
 }
 
-
-export default App
+export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,30 +1,29 @@
 import React from "react";
-import './App.css'
+import "./App.css";
 
 function Header() {
+  return (
+    <div className="display-flex flex-direction-column min-width-div-100vw min-height-div-14rem background-color-header color-white ">
+      <div className="display-flex flex-direction-column align-items-center max-width-35 rem margin-0-auto">
+        <h1 className="font-barlow fontsize-3rem ">Teknolojik Yemekler</h1>
 
-    return( 
-
-            <div className="display-flex flex-direction-column justify-content-center align-items-center min-width-div-100vw min-height-div-14rem background-color-header color-white ">
-                
-            <h1 className="font-barlow fontsize-4rem ">Teknolojik Yemekler</h1>
-
-            <nav>
-
-            <ul className="fontsize-header-ul display-flex gap-1rem list-style-type-none">
-                <li>
-                    <a className="text-decoration-none" href="">Anasayfa</a>
-                </li>
-                <li>
-                    <a className="text-decoration-none" href="">Sipariş Oluştur</a>
-                </li>
-            </ul>
-
-            </nav>
-
-            </div>
-
-    )
+        <nav>
+          <ul className="fontsize-header-ul display-flex gap-1rem list-style-type-none align-self-end">
+            <li>
+              <a className="text-decoration-none" href="">
+                Anasayfa
+              </a>
+            </li>
+            <li>
+              <a className="text-decoration-none" href="">
+                Sipariş Oluştur
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
